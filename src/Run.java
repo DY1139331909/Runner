f14b731f5a857b04ea53869224ed5db54e441ba0
howey <howeycheng@163.com>
2020-05-05 00:40:38 +0800

    完成caseDate类 初始化方法

--- src/Run.java
+++ src/Run.java
@@ -1,11 +1,14 @@
 import org.apache.rocketmq.client.exception.MQClientException;
 
+import java.util.Iterator;
+import java.util.Map;
+
 /**
  * Created by 程浩 on 2020/4/19
  */
 public class Run {
     public static void main(String[] args) {
-        String topic = "CASE3";
+        String topic = "CASE2";
         String nameSrvAddr = "127.0.0.1:9876";
         Listener listener = new Listener();
         try {
@@ -15,9 +18,14 @@ public class Run {
         }
 
     }
-
     public static void run(String caseData,String caseName){
         CaseData data = new CaseData();
         data.initCaseDataInput(caseData,caseName);
+        Iterator iterComp = data.caseDataInput.entrySet().iterator();
+        while (iterComp.hasNext()) {
+            Map.Entry entryComp = (Map.Entry) iterComp.next();
+            String compName = entryComp.getKey().toString();
+            System.out.println(compName);
+        }
     }
 }
