5d7187bad6f260621852f1e156aa98586fb344fd
DY1139331909 <44798627+DY1139331909@users.noreply.github.com>
2020-09-16 16:22:52 +0800

    Update CaseData.java
    增加CaseDate中putinto的实现方式

--- src/tools/CaseData.java
+++ src/tools/CaseData.java
@@ -87,6 +87,7 @@ public class CaseData {
     public void putInto(String field, Object values) {
         String[] valueArr = caseData.get(this.compName).get(field);
         valueArr[1] = values.toString();
+        
     }
 
     public String getFrom(String field) {
